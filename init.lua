44f20619562cd490bd0679f7e1291bcd124f9248
Nick Lowery <ClockVapor@users.noreply.github.com>
2022-07-11 15:34:45 -0600

    Initial commit

---
+++ init.lua
@@ -0,0 +1,24 @@
+vim.call('plug#begin', '~/.config/nvim/plugged')
+    local Plug = vim.fn['plug#']
+    Plug('preservim/nerdtree')
+    Plug('neoclide/coc.nvim', {branch = 'release' })
+    Plug 'cormacrelf/vim-colors-github'
+vim.call('plug#end')
+
+vim.g.mapleader = ','
+vim.api.nvim_set_keymap('n', '<Leader>l', ':<C-u>NERDTreeToggle<CR>', { noremap = true, silent = true })
+
+vim.cmd('color zellner')
+
+vim.opt.number = true
+vim.opt.wrap = true
+vim.opt.eol = true -- End text files with a newline
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
+
+vim.opt.foldmethod = 'indent'
+vim.opt.foldlevel = 99 -- Open all folds by default
