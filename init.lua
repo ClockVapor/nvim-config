82578dfc533413d75498deed25010d91bda45be9
Nick Lowery <ClockVapor@users.noreply.github.com>
2022-07-18 16:23:52 -0600

    coc bindings for jumping to errors, definitions, and references, and confirming autocomplete

--- init.lua
+++ init.lua
@@ -6,7 +6,8 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 vim.call('plug#end')
 
 vim.g.mapleader = ','
-vim.api.nvim_set_keymap('n', '<Leader>l', ':<C-u>NERDTreeToggle<CR>', { noremap = true, silent = true })
+-- Leader-f to toggle file tree panel
+vim.api.nvim_set_keymap('n', '<Leader>f', ':<C-u>NERDTreeToggle<CR>', { noremap = true, silent = true })
 
 -- Control-space to initialize coc autocomplete
 vim.api.nvim_set_keymap('i', '<c-space>', 'coc#refresh()', { noremap = true, silent = true, expr = true })
@@ -14,11 +15,21 @@ vim.api.nvim_set_keymap('i', '<c-space>', 'coc#refresh()', { noremap = true, sil
 vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? "<C-n>" : "<Tab>"', { noremap = true, silent = true, expr = true })
 -- Shift-tab to scroll up in coc autocomplete list
 vim.api.nvim_set_keymap('i', '<S-Tab>', 'pumvisible() ? "<C-p>" : "<S-Tab>"', { noremap = true, silent = true, expr = true })
+-- Navigate to next/previous error/warning
+vim.api.nvim_set_keymap('n', '<Leader>j', '<plug>(coc-diagnostic-next)', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>k', '<plug>(coc-diagnostic-prev)', { noremap = true, silent = true })
+-- Leader-d to jump to symbol definition
+vim.api.nvim_set_keymap('n', '<Leader>d', '<plug>(coc-definition)', { noremap = true, silent = true })
+-- Leader-r to list symbol references
+vim.api.nvim_set_keymap('n', '<Leader>r', '<plug>(coc-references)', { noremap = true, silent = true })
+-- Modifying <cr> to confirm autocomplete entry (including other edits like automatically adding imports)
+vim.api.nvim_set_keymap('i', '<cr>', 'pumvisible() ? "<C-y>" : "<C-g>u<cr>"', { noremap = true, silent = true, expr = true })
 
 vim.cmd('color zellner')
 
 vim.opt.number = true
 vim.opt.wrap = true
+vim.opt.linebreak = true
 vim.opt.breakindent = true
 vim.opt.showbreak = '    '
 vim.opt.eol = true -- End text files with a newline
